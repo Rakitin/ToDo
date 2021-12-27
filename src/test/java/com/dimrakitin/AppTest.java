3e8a302d41861aa44af74135a5ee556c52fe55c3
Dmitry Rakitin <dimrakitin@yandex.ru>
2021-12-27 13:43:12 +0300

    init

---
+++ src/test/java/com/dimrakitin/AppTest.java
@@ -0,0 +1,15 @@
+package com.dimrakitin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+	
+	@Test
+	void contextLoads() {
+//		 assertThat(greeter.get(), is("Hello, Boot!"));
+	}
+
+}
